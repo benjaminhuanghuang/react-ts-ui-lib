69da26ca7b911a06d30394177bcb5e55f48d3636
benjaminhuanghuang <bhuang@rms.com>
2020-08-09 10:02:58 -0700

    add icon

---
+++ _notes/05-icon.md
@@ -0,0 +1,13 @@
+## Setup
+```
+  npm i -S @fortawesome/fontawesome-svg-core
+  npm i -S @fortawesome/free-solid-svg-icons
+  npm i -S @fortawesome/react-fontawesome
+
+```
+
+## 二次封装，加入颜色
+
+
+
+## scss map and foreach
--- ben-ui-lib/src/App.tsx
+++ ben-ui-lib/src/App.tsx
@@ -1,17 +1,31 @@
 import React from "react";
-
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {fas} from '@fortawesome/free-solid-svg-icons'   // import all icons
 // Test
 import Button, { ButtonSize, ButtonType } from "./components/Button/button";
-import Menu from "./components/Menu/menu"
-import MenuItem from "./components/Menu/menuItem"
+import Menu from "./components/Menu/menu";
+import MenuItem from "./components/Menu/menuItem";
+import Icon from "./components/Icon/icon";
 
+
+library.add(fas)
 
 function App() {
   return (
     <div className="App">
-       <Menu defaultIndex='0' onSelect={(index)=>{alert(index)}} mode='vertical' defaultOpenSubMenus={['2']}>
+      <Icon icon="coffee" theme="danger" size="10x" />
+      <Menu
+        defaultIndex="0"
+        onSelect={(index) => {
+          alert(index);
+        }}
+        mode="vertical"
+        defaultOpenSubMenus={["2"]}
+      >
         <MenuItem index="0">Cool link</MenuItem>
-        <MenuItem disabled index="1">Cool link 2</MenuItem>
+        <MenuItem disabled index="1">
+          Cool link 2
+        </MenuItem>
         {/* <SubMenu title="dropdown">
           <MenuItem>dropdown 1</MenuItem>
           <MenuItem>dropdown 2</MenuItem>
@@ -19,12 +33,19 @@ function App() {
         <MenuItem index="2">Cool link 3</MenuItem>
       </Menu>
 
-      <Button disabled >Default</Button>
-      <Button btnType='primary' size="lg">Primary</Button>
-      <Button btnType='danger' size="sm">Primary</Button>
-      <Button btnType='link' href="http://google.com">Link</Button>
-      <Button btnType='link' href="http://google.com" disabled>DisabledLink</Button>
-
+      <Button disabled>Default</Button>
+      <Button btnType="primary" size="lg">
+        Primary
+      </Button>
+      <Button btnType="danger" size="sm">
+        Primary
+      </Button>
+      <Button btnType="link" href="http://google.com">
+        Link
+      </Button>
+      <Button btnType="link" href="http://google.com" disabled>
+        DisabledLink
+      </Button>
     </div>
   );
 }
